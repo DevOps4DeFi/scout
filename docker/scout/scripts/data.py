92274241704c0f4f1f8026ad3e7779e8ddaf4925
BenLeibig <ben.leibig@gmail.com>
2021-03-10 02:25:38 +0000

    change var names

--- docker/scout/scripts/data.py
+++ docker/scout/scripts/data.py
@@ -143,12 +143,12 @@ class Badgertree:
 
 sett_vaults = {
         "bBADGER"         : "0x19D97D8fA813EE2f51aD4B4e04EA08bAf4DFfC28",
-        "brenCrv"         : "0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545",
-        "bsbtcCrv"        : "0xd04c48A53c111300aD41190D63681ed3dAd998eC",
-        "btbtcCrv"        : "0xb9D076fDe463dbc9f915E5392F807315Bf940334",
-        "buniBadgerWbtc"  : "0x235c9e24D3FB2FAFd58a2E49D454Fdcd2DBf7FF1",
-        "bslpBadgerWbtc"  : '0x1862A18181346EBd9EdAf800804f89190DeF24a5',
-        "bharvestRenCrv"  : "0xAf5A1DECfa95BAF63E0084a35c62592B774A2A87",
+        "bcrvRenWBTC"         : "0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545",
+        "bcrvRenSWBTC"        : "0xd04c48A53c111300aD41190D63681ed3dAd998eC",
+        "bcrvTbtcSbtc"        : "0xb9D076fDe463dbc9f915E5392F807315Bf940334",
+        "buniWbtcBadger"  : "0x235c9e24D3FB2FAFd58a2E49D454Fdcd2DBf7FF1",
+        "bslpWbtcBadger"  : '0x1862A18181346EBd9EdAf800804f89190DeF24a5',
+        "bharvestcrvRenWBTC"  : "0xAf5A1DECfa95BAF63E0084a35c62592B774A2A87",
         "bDIGG"           : "0x7e7E112A68d8D2E221E11047a72fFC1065c38e1a",
         "buniDiggWbtc"    : "0xC17078FDd324CC473F8175Dc5290fae5f2E84714",
         "bslpDiggWbtc"  : "0x88128580ACdD9c04Ce47AFcE196875747bF2A9f6",
