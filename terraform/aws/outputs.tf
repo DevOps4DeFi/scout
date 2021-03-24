6520e9fa31b61dcf1f60e84b190729a6987602bf
BenLeibig <ben.leibig@gmail.com>
2021-03-25 00:17:06 +0100

    use app_name for name.

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -1,9 +1,9 @@
 output "scout_ecr_url" {
-  value = aws_ecr_repository.scout.repository_url
+  value = try(aws_ecr_repository.scout[0].repository_url, null)
 }
 output "grafana_ecr_url" {
-  value = aws_ecr_repository.grafana.repository_url
+  value = try(aws_ecr_repository.grafana[0].repository_url, null)
 }
 output "prometheus_ecr_url" {
-  value = aws_ecr_repository.prometheus.repository_url
+  value = try(aws_ecr_repository.prometheus[0].repository_url, null)
 }
